e20a520234532c377dbe826839fb1b23144bba75
cubexy <54584804+cubexy@users.noreply.github.com>
2021-02-02 20:36:15 +0100

    Update main.py

--- code/main.py
+++ code/main.py
@@ -31,7 +31,7 @@ def show_menu(width,height):
     draw = ImageDraw.Draw(image)
     draw.rectangle((9, 59, 56, 106), outline=0, fill=(255,255,255))
     draw.rectangle((71, 59, 119, 106), outline=0, fill=(255,255,255))
-    draw.multiline_text((20, 60), "Photo\nMode", fill=(0, 0, 0), )
+    draw.multiline_text((20, 70), "Photo\nMode", fill=(0, 0, 0), )
     draw.multiline_text((50, 50), "Webcam\nMode", fill=(0, 0, 0))
     disp.LCD_ShowImage(image , 0, 0)
     selector=-1
