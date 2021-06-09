eed3b77db4f01ec4f6b64d1441d615e1df09be94
cubexy <54584804+cubexy@users.noreply.github.com>
2021-06-09 10:23:37 +0200

    ms to s

--- code/main.py
+++ code/main.py
@@ -172,7 +172,7 @@ def mode_camera():
             tprint("Showing thumbnail")
             exec_end = datetime.now()
             exec_end.microsecond
-            exec_time = str((exec_end - exec_start).microseconds)
+            exec_time = str((exec_end - exec_start).microseconds*0.01)
             tprint("Image was rendered in " + exec_time + " ms")
             sleep(1.5)
             stream.flush()
